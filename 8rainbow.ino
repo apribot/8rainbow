55d35b694b98d7d0f26e19cf4ee3386c15d8d888
apribot <apribot@gmail.com>
2020-01-02 19:13:02 -0500

    Create 8rainbow.ino

---
+++ 8rainbow.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN    5
+#define LED_COUNT 8
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();           
+  strip.show();            
+  strip.setBrightness(50); 
+}
+
+void loop() {
+  rainbowCycle(100);    
+}
+
+
+
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
